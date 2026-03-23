50b9fe7f9ad6fd0a573084b322e293d750500489
Sujwal <dsujwal24@gmail.com>
2026-03-23 15:07:26 +0545

    LL Problems

--- 04_LinkedLists/01_fast_slow.js
+++ 04_LinkedLists/01_fast_slow.js
@@ -6,14 +6,20 @@ class ListNode {
 }
 
 // Create Linked List Manually
-// const first = new ListNode(1);
-// const second = new ListNode(2);
-// const third = new ListNode(3);
-// const fourth = new ListNode(4);
-// first.next = second;
-// second.next = third;
-// third.next = fourth;
-// const head = first;
+const first = new ListNode(1);
+const second = new ListNode(2);
+const third = new ListNode(3);
+const fourth = new ListNode(3);
+const fifth = new ListNode(4);
+const sixth = new ListNode(4);
+const seventh = new ListNode(5);
+first.next = second;
+second.next = third;
+third.next = fourth;
+fourth.next = fifth;
+fifth.next = sixth;
+sixth.next = seventh;
+const head = first;
 
 function createLinkedList(arr) {
   if (arr.length === 0) return null;
@@ -27,7 +33,7 @@ function createLinkedList(arr) {
   return head;
 }
 
-const head = createLinkedList([1, 2, 3, 4, 5]);
+// const head = createLinkedList([1, 2, 3, 4, 5]);
 // console.log(JSON.stringify(head, null, 1));
 
 function printList(head) {
@@ -40,5 +46,35 @@ function printList(head) {
   output += null;
   console.log(output);
 }
-printList(head);
+// printList(head);
 // Output: 1 -> 2 -> 3 -> 4 -> 5 -> null
+
+//
+
+// 82. Remove Duplicates from Sorted List II
+
+var deleteDuplicates = function (head) {
+  let dummyNode = new ListNode(0);
+  dummyNode.next = head;
+
+  let prev = dummyNode;
+  let curr = head;
+
+  while (curr) {
+    if (curr.next && curr.val === curr.next.val) {
+      let dupVal = curr.val;
+
+      while (curr && curr.val === dupVal) {
+        curr = curr.next;
+      }
+      prev.next = curr;
+    } else {
+      prev = curr;
+      curr = curr.next;
+    }
+  }
+  return dummyNode.next;
+};
+
+let deleteDuplicateskListII = deleteDuplicates(head);
+console.log(deleteDuplicateskListII);
